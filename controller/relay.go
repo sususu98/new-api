7e3537a23fcf1a323ea5183bc951bfa49e699f58
sususu <suchangshan@foxmail.com>
2025-11-20 12:18:03 +0800

    feat: Add support for count_tokens endpoint with free usage and no RPM tracking

--- controller/relay.go
+++ controller/relay.go
@@ -141,7 +141,12 @@ func Relay(c *gin.Context, relayFormat types.RelayFormat) {
 
 	// common.SetContextKey(c, constant.ContextKeyTokenCountMeta, meta)
 
-	if priceData.FreeModel {
+	// count_tokens 端点免费，不预扣费、不计费
+	isCountTokens := common.IsClaudeCountTokensPath(relayInfo.RequestURLPath)
+
+	if isCountTokens {
+		logger.LogInfo(c, "count_tokens 请求，跳过预扣费/计费")
+	} else if priceData.FreeModel {
 		logger.LogInfo(c, fmt.Sprintf("模型 %s 免费，跳过预扣费", relayInfo.OriginModelName))
 	} else {
 		newAPIError = service.PreConsumeQuota(c, priceData.QuotaToPreConsume, relayInfo)
